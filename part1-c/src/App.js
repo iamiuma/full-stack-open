0391657f588d83617b2be3f4b3a45bf19248e9a5
iuma <iulian.magherca@gmail.com>
2021-08-23 22:18:17 +0300

    part1-c finished

--- part1-c/src/App.js
+++ part1-c/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react'
-
-const Hello = (props) => {
-  return (
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old!
-      </p>
-    </div>
-  )
-}
+import React, { useState } from 'react'
 
 const App = () => {
-  const name = 'Peter'
-  const age = 40
+  const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
-      <h1>Greetings!</h1>
-      <Hello name = "George" age = {26 + 10} />
-      <Hello name = {name} age = {age} />
+      <Display counter={counter} />
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />
     </div>
   )
 }
+
+const Display = ({ counter }) => <div>{counter}</div>
+
+const Button = ({ onClick, text}) => (
+    <button onClick={onClick}>
+      {text}
+    </button>
+)
 
 export default App

815c1dfbfb6c5548ace285fcc36d4182ccb56d2e
iuma <iulian.magherca@gmail.com>
2021-08-26 13:09:32 +0300

    part1-d-handling_arrays

--- part1-d/src/App.js
+++ part1-d/src/App.js
@@ -1,26 +1,30 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left: 0, right: 0
-  })
 
-  const handleLeftClicks = () =>
-    setClicks({ ...clicks, left: clicks.left + 1 })
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const handleRightClicks = () =>
-    setClicks({ ...clicks, right: clicks.right + 1 })
+  const handleLeftClick = () =>
+    setLeft(left + 1)
+    setAll(allClicks.concat('L'))
+
+  const handleRightClick = () =>
+    setRight(right + 1)
+    setAll(allClicks.concat('R'))
 
   return (
     <div>
-      {clicks.left}
-      <button onClick={handleLeftClicks}>
+      {left}
+      <button onClick={handleLeftClick}>
         left
       </button>
-      <button onClick={handleRightClicks}>
+      <button onClick={handleRightClick}>
         right
       </button>
-      {clicks.right}
+      {right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
